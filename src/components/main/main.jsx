c5ccc0616346c7b950b7fcfb3b9e76bc87cfa655
Sursh <6990413@mail.ru>
2020-02-25 08:46:16 +0300

    v 5.2.1

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,6 +4,7 @@ import OffersList from '../offers-list/offers-list';
 import OffersMap from '../offers-map/offers-map';
 import {connect} from 'react-redux';
 import OffersCities from '../offers-cities/offers-cities';
+import OffersFilter from '../offers-filter/offers-filter';
 
 const Main = (props) => {
   const {cards, citiesNames, city} = props;
@@ -60,29 +61,7 @@ const Main = (props) => {
               <b className="places__found">
                 {cards.length > 0 ? cards.length : 0} places to stay in {city}
               </b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex="0">
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex="0">
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex="0">
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <OffersFilter />
               <div className="cities__places-list places__list tabs__content">
                 <OffersList cards={cards} onHeaderClick={_cardHeaderClickHandler} />
               </div>
@@ -106,13 +85,28 @@ Main.propTypes = {
 
 const mapStateToProps = (state, props) => {
   let city = state.city;
+  const urlSearch = new URLSearchParams(props.location.search);
+  const newCity = urlSearch.get(`city`);
+  const filter = urlSearch.get(`filter`);
+  if (newCity) {
+    city = newCity;
+  }
+  let cards = state.offers.filter((offer) => offer.city === city);
 
-  if (props.match.params.city) {
-    city = props.match.params.city;
+  switch (filter) {
+    case `lowToHigh`:
+      cards = cards.sort((card1, card2) => card1.price - card2.price);
+      break;
+    case `highToLow`:
+      cards = cards.sort((card1, card2) => card2.price - card1.price);
+      break;
+    case `topRated`:
+      cards = cards.sort((card1, card2) => card2.avgMark - card1.avgMark);
+      break;
   }
 
   return {
-    cards: state.offers.filter((offer) => offer.city === city),
+    cards,
     city,
     citiesNames: state.citiesNames
   };
---
+++ src/components/offers-filter/offers-filter.jsx
@@ -0,0 +1,99 @@
+import React, {PureComponent} from 'react';
+import {withRouter} from 'react-router';
+
+const FILTERS = [
+  {name: `popular`, value: `Popular`},
+  {name: `lowToHigh`, value: `Price: low to high`},
+  {name: `highToLow`, value: `Price: high to low`},
+  {name: `topRated`, value: `Top rated first`}
+];
+
+class OffersFilter extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeFilter: `popular`,
+      activeFilterValue: `Popular`,
+      isOpen: false,
+      city: ``
+    };
+
+    this._filterOpenHandler = this._filterOpenHandler.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      activeFilter: `popular`,
+      activeFilterValue: `Popular`,
+      isOpen: false
+    });
+  }
+
+  componentDidUpdate() {
+    const newCity = new URLSearchParams(this.props.location.search).get(`city`);
+
+    if (newCity !== this.state.city) {
+      this.setState({
+        activeFilter: `popular`,
+        activeFilterValue: `Popular`,
+        city: newCity
+      });
+    }
+  }
+
+  _setActiveFilter(filter) {
+    this.setState({
+      activeFilter: filter.name,
+      activeFilterValue: filter.value,
+      isOpen: false
+    });
+    const city = new URLSearchParams(this.props.location.search).get(`city`);
+    if (city) {
+      this.setState({
+        city: city
+      });
+    }
+    this.props.history.push(`${city ? `?city=${city}&` : `?`}filter=${filter.name}`);
+  }
+
+  _filterOpenHandler() {
+    this.setState((prevState) => ({isOpen: !prevState.isOpen}));
+  }
+
+  _renderFilters() {
+    return FILTERS.map((filter, index) => (
+      <li
+        className={`places__option ${filter.name === this.state.activeFilter &&
+        `places__option--active`}`}
+        tabIndex="0"
+        onClick={() => this._setActiveFilter(filter)}
+        key={`${filter.name} - ${index}`}
+      >
+        {filter.value}
+      </li>
+    ));
+  }
+
+  render() {
+    return (
+      <form className="places__sorting" action="#" method="get">
+        <span className="places__sorting-caption">Sort by</span>
+        <span className="places__sorting-type" tabIndex="0" onClick={this._filterOpenHandler}>
+          {this.state.activeFilterValue}
+          <svg className="places__sorting-arrow" width="7" height="4">
+            <use xlinkHref="#icon-arrow-select"></use>
+          </svg>
+        </span>
+        <ul
+          className={`places__options places__options--custom places__options--${
+            this.state.isOpen ? `opened` : `custom`
+          }`}
+        >
+          {this._renderFilters()}
+        </ul>
+      </form>
+    );
+  }
+}
+
+export default withRouter(OffersFilter);
